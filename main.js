06c1508c4819ca68e5241930b728bee46a46d02c
Jedi992 <johndoe@example.com>
2024-09-26 11:02:18 +0300

    1

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-addEventListener("input", (event) => {
-  console.log(event);
-});
+const ul = document.querySelector(".nav__list-ul");
+const addButton = document.querySelector(".todo__add-button");
+const input = document.querySelector(".todo__text-input").value;
+const nav = document.querySelector(".nav__list");
+const todoArr = [];
+
+console.log(input);
+
+document.createElement("li");
+
+function addTodo() {
+  console.log(document.body);
+}
+
+addButton.addEventListener("click", addTodo);
